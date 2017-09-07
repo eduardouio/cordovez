060a972e1694efe4b6808073738ca1a85574ab27
Eduardo Villota <eduardouio7@gmail.com>
2017-09-07 17:52:34 -0500

    Carga de librerias angula

---
+++ APPImportaciones/app/js/app/pedidos/app.js
@@ -0,0 +1,12 @@
+/**
+ * Modulo encargado de manejar los pedidos de acuerdo al archivo de especificaciones
+ *
+ * @package    CordovezApp JS
+ * @author    Eduardo Villota <eduardouio7@gmail.com>
+ * @copyright    Copyright (c) 2014,  Agencias y Representaciones Cordovez S.A.
+ * @license    Todos los derechos reservados Agencias y Representaciones Cordovez S.A.
+ * @link    https://github.com/eduardouio/APPImportaciones
+ * @since    Version 1.0.0
+ * @filesource
+ */
+var cordovezApp = angular.module('cordovezApp' , ['ngRoute',])

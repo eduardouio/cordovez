29721aeec22cfb8577d4e18772a2ca0b07181ddf
Eduardo Villota <eduardouio7@gmail.com>
2017-09-26 20:03:41 -0500

    Se separan los controllers para manejar los modulos por separadas, submodulos con tareas especificos

---
+++ APPImportaciones/app/js/app/controllers/presentarPedidoController.js
@@ -0,0 +1,25 @@
+cordovezApp.controller('presentarPedidoController', [ 
+                                              '$scope', 
+                                              '$location',
+                                              '$routeParams',
+                                              '$timeout',
+                                              'factoryLogin',
+                                              'pedidoFactory',
+                                              'gastosinicialesFactory',
+                                              'incotermsFactory',
+                                      function(
+                                              $scope, 
+                                              $location,
+                                              $routeParams,
+                                              $timeout,
+                                              factoryLogin,
+                                              pedidoFactory,
+                                              detallePedidoFactory, 
+                                              gastosinicialesFactory,
+                                              incotermsFactory,
+                                        ) {
+
+console.log('[Debug] presentarPedidoController Loaded');
+
+
+}]);

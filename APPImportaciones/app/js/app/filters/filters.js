d664cd52b305081be9a6119f06b966c5666e1ed3
Eduardo Villota <eduardouio7@gmail.com>
2017-10-05 22:03:54 -0500

    Base app sin Angular

--- APPImportaciones/app/js/app/filters/filters.js
+++
@@ -1,8 +0,0 @@
-cordovezApp.filter('dateDiff', ['$filter' , function($filter){
-	return function(endDate, startDate){
-		if(endDate == null || startDate == null){return '';}
-			var start = moment(startDatet);
-			var end = moment(endDate);
-			return end.diff(start, ' dias');
-	};
-}]);

8ef51748b9efe1759f669f0a67961e1df913de6e
Eduardo Villota <eduardouio7@gmail.com>
2017-12-27 00:03:26 -0500

    se elimina la carpeta con el framework de pruebas unitarias

---
+++ app/src/tests/FacinformativaTest.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * Facinformativa test case.
+ */
+class FacinformativaTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     * @var Facinformativa
+     */
+    private $facinformativa;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        // TODO Auto-generated FacinformativaTest::setUp()
+        
+        $this->facinformativa = new Facinformativa(/* parameters */);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated FacinformativaTest::tearDown()
+        $this->facinformativa = null;
+        
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+
+    /**
+     * Tests Facinformativa->__construct()
+     */
+    public function test__construct()
+    {
+        // TODO Auto-generated FacinformativaTest->test__construct()
+        $this->markTestIncomplete("__construct test not implemented");
+        
+        $this->facinformativa->__construct(/* parameters */);
+    }
+
+    /**
+     * Tests Facinformativa->index()
+     */
+    public function testIndex()
+    {
+        // TODO Auto-generated FacinformativaTest->testIndex()
+        $this->markTestIncomplete("index test not implemented");
+        
+        $this->facinformativa->index(/* parameters */);
+    }
+
+    /**
+     * Tests Facinformativa->presentar()
+     */
+    public function testPresentar()
+    {
+        // TODO Auto-generated FacinformativaTest->testPresentar()
+        $this->markTestIncomplete("presentar test not implemented");
+        
+        $this->facinformativa->presentar(/* parameters */);
+    }
+
+    /**
+     * Tests Facinformativa->nuevo()
+     */
+    public function testNuevo()
+    {
+        // TODO Auto-generated FacinformativaTest->testNuevo()
+        $this->markTestIncomplete("nuevo test not implemented");
+        
+        $this->facinformativa->nuevo(/* parameters */);
+    }
+
+    /**
+     * Tests Facinformativa->editar()
+     */
+    public function testEditar()
+    {
+        // TODO Auto-generated FacinformativaTest->testEditar()
+        $this->markTestIncomplete("editar test not implemented");
+        
+        $this->facinformativa->editar(/* parameters */);
+    }
+
+    /**
+     * Tests Facinformativa->eliminar()
+     */
+    public function testEliminar()
+    {
+        // TODO Auto-generated FacinformativaTest->testEliminar()
+        $this->markTestIncomplete("eliminar test not implemented");
+        
+        $this->facinformativa->eliminar(/* parameters */);
+    }
+
+    /**
+     * Tests Facinformativa->validar()
+     */
+    public function testValidar()
+    {
+        // TODO Auto-generated FacinformativaTest->testValidar()
+        $this->markTestIncomplete("validar test not implemented");
+        
+        $this->facinformativa->validar(/* parameters */);
+    }
+}
+

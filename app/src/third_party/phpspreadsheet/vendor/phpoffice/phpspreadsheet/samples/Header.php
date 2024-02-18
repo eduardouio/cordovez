a759a81242836551757ef74201b29de76d85429a
Eduardo Villota <eduardouio7@gmail.com>
2024-02-18 13:09:27 -0500

    Actualizados los enlaces de los scripts de Bootstrap en Header.php

--- app/src/third_party/phpspreadsheet/vendor/phpoffice/phpspreadsheet/samples/Header.php
+++ app/src/third_party/phpspreadsheet/vendor/phpoffice/phpspreadsheet/samples/Header.php
@@ -24,8 +24,8 @@ if ($helper->isCli()) {
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/bootstrap/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="/bootstrap/css/phpspreadsheet.css"/>
-    <script src="/bootstrap/js/jquery.min.js"></script>
-    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script>
     </script>
 </head>

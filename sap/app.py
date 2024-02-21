ac4fc401554eba33764c786de5db1e8eedccb6ba
Eduardo Villota <eduardouio7@gmail.com>
2024-02-21 17:10:03 -0500

    refactor

--- sap/app.py
+++ sap/app.py
@@ -6,27 +6,30 @@ import json
 from Model import Model
 from ModelProducts import ModelProducts
 
+
 @get('/<enterprise>/<year>/')
 def getOrders(enterprise, year):
     response.headers['Content-Type'] = 'application/json'
     response.headers['Cache-Control'] = 'no-cache'
-    model_db = Model(enterprise,year)
+    model_db = Model(enterprise, year)
     data = model_db.get_data()
-    return json.dumps({'data' : data })
+    return json.dumps({'data': data})
+
 
 @get('/')
 def test():
     response.headers['Content-Type'] = 'application/json'
     response.headers['Cache-Control'] = 'no-cache'
     return json.dumps({
-        'data':'Test',
-        'urls' : [
+        'data': 'Test',
+        'urls': [
             'cordovez/YYYY/',
             'imnac/YYYY/',
             'vid/YYYY/',
         ],
         'comments': 'try change the url'
-        })
+    })
+
 
 @get('/<enterprise>/')
 def getProducts(enterprise):
@@ -40,23 +43,25 @@ def getProducts(enterprise):
     """
     response.headers['Content-Type'] = 'application/json'
     response.headers['Cache-Control'] = 'no-cache'
-    products  = ModelProducts(enterprise).get_all()
+    products = ModelProducts(enterprise).get_all()
     return json.dumps({
         'data': products
     })
 
+
 class StripPathMiddelware(object):
     '''Get taht slash out of request '''
 
-    def __init__(self,a):
+    def __init__(self, a):
         self.a = a
 
-    def __call__(self,e,h):
+    def __call__(self, e, h):
         e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
-        return self.a(e,h)
+        return self.a(e, h)
+
 
 if __name__ == '__main__':
     ''' run server configuration '''
     run(host='0.0.0.0',
         port=8000
-            )
+        )
